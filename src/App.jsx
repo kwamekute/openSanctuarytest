ea32ee673665fecbc238ed1d7a79b1b0cee04999
kwamekute <thatkutekwame@gmail.com>
2025-07-25 22:02:33 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold text-blue-600 p-4">
+        OpenSanctuary is working!
+      </div>
+      <div className="border-red w-20 h-20">
+
+      </div>
+    </>
+  )
+}
+
+export default App
